a28b02a308a44a24502c892ada24b0a907ae1bf7
dedebono <wartanewsgo2@gmail.com>
2025-07-02 21:17:37 +0800

    new feature: event, sermon, photo, IP

--- src/pages/admin/api/API.js
+++ src/pages/admin/api/API.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // List of backends to failover between
 const backends =
   process.env.NODE_ENV === 'development'
-    ? ['http://localhost:10000']
+    ? ['https://server.dedebono.uk']
     : [
         'https://church-backend-no8q.onrender.com', // primary
         'https://server.dedebono.uk',               // fallback
